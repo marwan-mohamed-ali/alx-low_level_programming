b31bc9561f8d799159dc0bd6547ff553afab2ef5
Marwan <marwanmohamedali01@gmail.com>
2023-03-20 09:40:33 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print charctar using write function
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1.quo.59);
+	return (1);
+}
