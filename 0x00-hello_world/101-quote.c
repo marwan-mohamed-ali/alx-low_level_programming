57716760fe50e929c49753deed2a3035bb19315a
Marwan <marwanmohamedali01@gmail.com>
2023-03-20 09:43:57 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
  * Description: A C program that print charctar using write function
  *
  * Return: Always 1 (Success)
- */
+*/
 
 int main(void)
 {
 char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1.quo.59);
+write(1, que, 59);
 	return (1);
 }
