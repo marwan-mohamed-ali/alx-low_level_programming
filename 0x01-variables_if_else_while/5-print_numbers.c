6ac941dfd6e0b2a7efecdbafdc85ef07b093aadd
Marwan <marwanmohamedali01@gmail.com>
2023-03-20 14:34:52 -0700

    01 2 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Description: A C program that preints  prints all single digit numbers.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int N = 0;
+
+	while (N < 10)
+{
+	printf("%i", N);
+	N++;
+}
+	printf("\n");
+	return (0);
+}
