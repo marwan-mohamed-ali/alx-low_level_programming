403b506745602d7ca6cc525b343d3c46b4408dc8
Marwan <marwanmohamedali01@gmail.com>
2023-03-23 23:57:53 -0700

    aa

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
