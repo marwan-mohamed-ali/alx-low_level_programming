456591df5d8fdcd73ff4c437d2a8585808c344ad
Marwan <marwanmohamedali01@gmail.com>
2023-03-24 00:40:07 -0700

    df

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+#include <stdio.h>
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,7 +14,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
 
 #endif
-}
